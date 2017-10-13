e6bb8f8552a26ad4e51f0210eddc086bde5924cd
Anton <antongorynya1@gmail.com>
2017-10-13 11:50:40 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -3,35 +3,41 @@ from os.path import join, getsize
 import argparse
 
 
-def extract_list_of_files(directory,list_of_file = []):
+def extract_list_of_files(directory, list_of_file = []):
     for dir_entry in os.scandir(directory):
         if dir_entry.is_dir():
-           extract_list_of_files(dir_entry,list_of_file)
+           extract_list_of_files(dir_entry, list_of_file)
         else:
-            list_of_file.append({dir_entry.name:{'size':getsize(dir_entry.path),'path':dir_entry.path}})
+            list_of_file.append({'name': dir_entry.name,
+                                  'size': getsize(dir_entry.path),
+                                  'path': dir_entry.path})
     os.scandir(directory).close()
-    return  list_of_file
+    return list_of_file
 
 def remove_duplicates(directory):
+    removeble_files = []
     list_of_file = extract_list_of_files(directory)
     for position, file in enumerate(list_of_file):
-        name = list(file.keys())[0]
-        size = file[name]['size']
+        name = list(file['name'])
+        size = file['size']
         for position2, file in enumerate(list_of_file):
-            name2 = list(file.keys())[0]
-            size2 = file[name2]['size']
+            name2 = list(file['name'])
+            size2 = file['size']
             if (name == name2) and (size == size2) and (position2 > position):
-                os.remove(file[name2]['path'])
-                print ("remove ",file[name2]['path'])
+                removeble_files.append(file['path'])
+    for file_path in set(removeble_files):
+        os.remove(file_path)
+    return set(removeble_files)
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description= 'Remove duplicates')
-    parser.add_argument("directory", nargs=1, help="path to checking folder")
+    parser = argparse.ArgumentParser(description='Remove duplicates')
+    parser.add_argument("directory", help="path to checking folder")
     return parser
 
 if __name__ == '__main__':
     parser = create_parser()
     namespace = parser.parse_args()
     if namespace.directory:
-        remove_duplicates(namespace.directory[0])
+        for file_path in remove_duplicates(namespace.directory):
+            print ("remove", file_path)

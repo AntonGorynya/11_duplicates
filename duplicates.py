9eb14dc3df12e241e908aa3b45c5f0d36364b1d5
Anton <antongorynya1@gmail.com>
2017-10-18 11:36:13 +0300

    pep8

--- duplicates.py
+++ duplicates.py
@@ -6,11 +6,12 @@ import argparse
 def extract_list_of_files(directory, list_of_file=[]):
     for dir_entry in os.scandir(directory):
         if dir_entry.is_dir():
-            extract_list_of_files(dir_entry.path, list_of_file)
+            extract_list_of_files(dir_entry, list_of_file)
         else:
             list_of_file.append({'name': dir_entry.name,
                                 'size': getsize(dir_entry.path),
                                  'path': dir_entry.path})
+    os.scandir(directory).close()
     return list_of_file
 
 
@@ -18,12 +19,13 @@ def remove_duplicates(directory):
     removeble_files = []
     list_of_file = extract_list_of_files(directory)
     for position, file in enumerate(list_of_file):
-        name = list(file['name'])
+        file_name = list(file['name'])
         size = file['size']
         for position2, file in enumerate(list_of_file):
-            name2 = list(file['name'])
+            file_name2 = list(file['name'])
             size2 = file['size']
-            if (name == name2) and (size == size2) and (position2 > position):
+            if (file_name == file_name2) and (size == size2) \
+                    and (position2 > position):
                 removeble_files.append(file['path'])
     for file_path in set(removeble_files):
         os.remove(file_path)
@@ -38,6 +40,5 @@ def create_parser():
 if __name__ == '__main__':
     parser = create_parser()
     namespace = parser.parse_args()
-    if namespace.directory:
-        for file_path in remove_duplicates(namespace.directory):
-            print ("remove", file_path)
+    for file_path in remove_duplicates(namespace.directory):
+        print ("remove", file_path)
